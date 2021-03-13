a41427e24632bfc75e9b190bf4b6d8d0bbfd2288
robergi1 <robergi1@tlu.ee>
2021-03-13 11:22:51 +0200

    Add project files.

---
+++ XamCam/XamCam.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Plugin.Media;
+
+namespace XamCam.Droid
+{
+    [Activity(Label = "XamCam", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            await CrossMedia.Current.Initialize();
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ XamCam/XamCam/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using Xamarin.Forms;
+
+namespace XamCam
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, true);
+            this.Title = "Meie vägev kaamera";
+        }
+
+        private async void BtnCam_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                var photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
+                {
+                    DefaultCamera = Plugin.Media.Abstractions.CameraDevice.Rear,
+                    Directory = "Xamarin",
+                    SaveToAlbum = true
+                });
+                
+                if (photo != null)
+                    imgCam.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message.ToString(), "Ok");
+            }
+        }
+
+        private void BtnSave_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
